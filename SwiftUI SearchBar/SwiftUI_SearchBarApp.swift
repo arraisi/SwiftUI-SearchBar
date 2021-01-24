9f535ea03c5b233e6c2b01b8c62f6fdfaeb5166e
Abdul R. Arraisi <itsarraisi@gmail.com>
2021-01-24 18:01:47 +0700

    Initial Commit

---
+++ SwiftUI SearchBar/SwiftUI_SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SearchBarApp.swift
+//  SwiftUI SearchBar
+//
+//  Created by Abdul R. Arraisi on 24/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
